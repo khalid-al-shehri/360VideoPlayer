69aa8fc8128360b8506cad4563013a62830a3f4f
khalid-al-shehri <k.shehri@outlook.com>
2020-05-10 20:13:24 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:video_player_360/video_player_360.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('360 Video Player Flutter'),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () async {
+              await VideoPlayer360.playVideoURL(
+                  "assets/gb.mp4");
+            },
+            child: Text("Click to play Video URL"),
+          ),
+        ),
+      ),
+    );
+  }
+}
